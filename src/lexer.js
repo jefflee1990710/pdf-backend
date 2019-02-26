e49d0bbc3e94c3b47c18099d5fa30a26916c5e21
Jeff Lee <jefflee1990710@gmail.com>
2019-02-26 16:34:06 +0800

    Add basic structure on lexer.js

--- src/lexer.js
+++ src/lexer.js
@@ -16,4 +16,28 @@ export default class Lexer {
         return this.bufferStream.peekByte()
     }
 
+    getNumber(){
+        // to be implement
+    }
+
+    getString(){
+        // to be implement
+    }
+
+    getName(){
+        // to be implement
+    }
+
+    getHexString(){
+        // to be implement
+    }
+
+    getObj(){
+        // to be implement
+    }
+
+    skipToNextLine(){
+        // to be implement
+    }
+
 }
